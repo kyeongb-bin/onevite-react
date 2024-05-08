4070a3021f9b398e4139509a76e2e65cfb202176
kyeongb-bin <kyeongb0315@gmail.com>
2024-05-08 12:13:44 +0900

    1 push

---
+++ section01/chapter05.js
@@ -0,0 +1,26 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -28;
+
+let inf = Infinity; // 양의 무한대
+let mInf = -Infinity; // 음의 무한대
+
+let nan = NaN; // 수치연산 실패 결과값
+
+// 2. String Type
+let myName = "김경빈";
+let myLocation = "역곡";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다.`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다.)
+let empty = null;
+
+// 5. Undefined Type
+let none;
